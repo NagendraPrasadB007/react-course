9c7d579a939a7357165ff198fc6de126a25e86f6
NagendraPrasadB007 <nagendra7prasad2000@gmail.com>
2024-03-22 06:01:02 +0530

    passwordGenerator project

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="w-full h-screen duration-200"
       style={{ backgroundColor: color }} >
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-4">
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl">
           <button onClick={() => setColor("red")} className="outline-none px-4 py-2 rounded-full text-white shadow-lg" style={{ background: "red" }}>
             Red
